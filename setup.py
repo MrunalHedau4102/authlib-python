ab307902ce58f8dfe6dd9344e433741485c39540
MrunalHedau4102 <mrunalhedau2002@gmail.com>
2026-03-12 00:21:04 +0530

    change libr name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="authlib",
+    name="authlib-python",
     version="1.0.0",
     packages=find_packages(),
     python_requires=">=3.10",
